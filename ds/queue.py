94fb590b08b777a56909495c8c82fe3610554841
Pranav Garg <pranavfunny1@gmail.com>
2021-05-29 21:03:16 +0530

    fix: optimed_the_code

--- ds/queue.py
+++ ds/queue.py
@@ -42,9 +42,10 @@ class Queue():
     def deQueue(self):
         if self.isEmpty():
             print("Queue is already Empty, nothing to remove")
-            return
+            return -1000
         
         ele = self.arr[self.front]
+        self.arr[self.front] = -1
         self.front += 1
         if self.front > self.rear:
             self.front = self.rear = -1
@@ -78,9 +79,52 @@ class CircularQueue():
         self.Q = [-1] * size
         self.rear = self.front = -1
         self.capacity = size
+        self.size = 0
 
 
     def isEmpty(self):
         if self.rear == -1:
             return True
         return False
+
+    def length(self):
+        return self.size
+
+    def isFull(self):
+        if self.size == self.capacity:
+            return True
+        return False
+
+    def enQueue(self, ele):
+        if self.isFull():
+            print("Queue is already full, cant put any element in it")
+            return
+
+        if self.rear == -1:
+            self.rear = self.front = 0
+        else:
+            self.rear = (self.rear + 1) % self.capacity
+        self.Q[self.rear] = ele
+        self.size += 1
+
+    def deQueue(self):
+        if self.isEmpty():
+            print("Queue is already Empty")
+            return -1000
+        
+        ele = self.Q[self.front]
+        self.Q[self.front] = -1
+        self.front = (self.front + 1) % self.capacity
+        if (self.front) % self.capacity == (self.rear + 1) % self.capacity:
+            self.front = self.rear = 1
+        return ele
+
+    def printQueue(self):
+        if self.isEmpty():
+            print("Queue is already Empty, cannot print anything")
+            return
+        print("QUEUEUUE")
+        for i in range(0, self.capacity):
+            print(self.Q[i])
+
+        print("-"*10)
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from ds.stack import Stack
-from ds.queue import Queue
+from ds.queue import Queue, CircularQueue
 
 """s1 = Stack(2)
 print(s1.length())
@@ -14,7 +14,7 @@ print(s1.isFull())"""
 
 
 ## this chunk is for the implementation of Lineara queue
-q1 = Queue(3)
+"""q1 = Queue(3)
 
 print(q1.length())
 q1.enQueue(6)
@@ -24,3 +24,13 @@ print(q1.length())
 ans = q1.deQueue()
 print("ans",ans)
 q1.printQueue()
+"""
+
+q2 = CircularQueue(5)
+
+q2.enQueue(25)
+q2.enQueue(26)
+e1 = q2.deQueue()
+print(e1)
+q2.enQueue(39)
+q2.printQueue()
